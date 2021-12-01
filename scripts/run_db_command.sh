683887d30ac35dd56c8cb35143134d47286c721b
Joel Mbiye <joembiye@gmail.com>
2021-12-01 14:37:25 +0100

    Refactored to three microservices

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -1,9 +1,9 @@
-# Usage: pass in the DB container ID as the argument
+# Usage: pass in the DB container ID as the argument (same)
 
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
-export CT_DB_NAME=geoconnections
 
+export CT_DB_NAME=geoconnections
 
 cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
